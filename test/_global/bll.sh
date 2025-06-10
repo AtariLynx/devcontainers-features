fd121efd197355071ffffdc2ebef5e6325d250cc
Alex Thissen <athissen@killer-apps.nl>
2025-06-10 21:53:20 +0000

    BLL script edits

--- test/_global/bll.sh
+++ test/_global/bll.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # The 'test/_global' folder is a special test folder that is not tied to a single feature.
-#
-# This test file is executed against a running container constructed
-# from the value of 'color_and_hello' in the tests/_global/scenarios.json file.
-#
 # The value of a scenarios element is any properties available in the 'devcontainer.json'.
 # Scenarios are useful for testing specific options in a feature, or to test a combination of features.
 # 
@@ -13,7 +9,7 @@
 
 set -e
 
-# Optional: Import test library bundled with the devcontainer CLI
+# Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
 # Feature-specific tests
@@ -23,6 +19,7 @@ check "Check lynxenc installation" bash -c "lynxenc | grep 'usage: lynxenc <plai
 check "Check lynxdec installation" bash -c "lynxdec | grep 'usage: lynxdec <encrypted.bin> <plaintext.bin>'"
 check "Check make_lnx installation" bash -c "make_lnx 2>&1 | grep 'Raw image to LNX image convertor V5'"
 
+# Sceanrio test using lyxass, new_bll, make_lnx and lynxenc
 cd ${BLL_ROOT}/uBLL
 check "Compile uBLL" bash -c "make | grep 'DONE: File converted'"
 
